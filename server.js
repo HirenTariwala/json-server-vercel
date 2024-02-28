f8fedd945dec08882f561ffe917e3afa7751ca18
Hiren Tariwala <41938818+HirenTariwala@users.noreply.github.com>
2024-02-28 09:39:13 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
+// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-const auth = require('json-server-auth')
 
-const app = jsonServer.create()
+const server = jsonServer.create()
 
-const router = jsonServer.router('db/db.json')
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
 
-app.use(jsonServer.bodyParser)
-app.db = router.db
+// Comment out to allow write operations
+// const router = jsonServer.router('db.json')
 
-const rules = auth.rewriter({
-  users: 600,
-});
+const middlewares = jsonServer.defaults()
 
-app.use(rules)
-app.use(auth)
-app.use(router)
-app.listen(4000, () => {
+server.use(middlewares)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+  'users': 600
+}))
+server.use(router)
+server.listen(4000, () => {
     console.log('JSON Server is running')
 })
 
 // Export the Server API
-module.exports = app
+module.exports = server
